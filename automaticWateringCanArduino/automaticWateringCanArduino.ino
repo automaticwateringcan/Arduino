397b76ed4661aee47c25f4f97729d92e3fdabaa8
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-05-16 03:10:38 +0200

    watering plant & updating db via endpoint

--- automaticWateringCanArduino/automaticWateringCanArduino.ino
+++ automaticWateringCanArduino/automaticWateringCanArduino.ino
@@ -6,6 +6,14 @@
 #define dht_apin A1 // Analog Pin sensor is connected to
 #define RX 10
 #define TX 11
+#define WATER_PIN 7
+
+
+SoftwareSerial esp8266(RX, TX);
+
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+
 
 String WIFI_SSID = "molion";            // Your WiFi ssid
 String PASSWORD = "jasiu_luz777";       // Password
@@ -14,71 +22,103 @@ String HOST = "192.168.216.221";
 String PATH = "/api/plants/updateSensor";
 String PORT = "8080";
 
+
 int countTrueCommand;
 int countTimeCommand;
 boolean found = false;
 
+
+// that defines the equipment and plant id
 int idPlant = 5;
 
-int valSensor = 0;
 
-dht DHT;
-
-SoftwareSerial esp8266(RX, TX);
-
-StaticJsonBuffer<200> jsonBuffer;
-JsonObject& root = jsonBuffer.createObject();
-
+struct PlantData {
+  int soilMoistureSensorValue;
+  dht DHT;
+};
 
 void setup() {
   Serial.begin(9600);
   esp8266.begin(115200);
 
+  // set water pump
+  pinMode(WATER_PIN, OUTPUT);
+  digitalWrite(WATER_PIN, HIGH);
+
+  connectToInternet();
+
+  countTrueCommand = 0;
+}
+
+void connectToInternet() {
   sendCommand("AT",5,"OK");
   sendCommand("AT+CWMODE=1",5,"OK");
   sendCommand("AT+CWJAP=\""+ WIFI_SSID +"\",\""+ PASSWORD +"\"",20,"OK");
-
-  countTrueCommand = 0;
-
-  pinMode(7, OUTPUT);
-  digitalWrite(7, HIGH);
 }
 
 void loop() {
-
-  int soilMoistureSensorValue = analogRead(A0);
   
-  DHT.read11(dht_apin);
+  struct PlantData plantData;
+  plantData = gatherData();
+  
+  String data;
+  data = buildJson(plantData);
+  
+  String putRequest;
+  putRequest = preparePutRequest(data);
 
+  sendPutRequest(putRequest);
+
+//  boolean clientStatus = getClientStatus();
+
+  if (plantData.soilMoistureSensorValue < 35) {
+    water();
+  } 
+//  else if(clientStatus) {
+//    water();
+//  }
+}
+
+
+struct PlantData gatherData() {
+  struct PlantData plantData;
+
+  plantData.soilMoistureSensorValue = ((analogRead(A0) * 100 / 700) - 100) * (-1);
+
+  plantData.DHT.read11(dht_apin);
+  
+  return plantData;
+}
+
+String buildJson(struct PlantData plantData) {
   root["id"] = idPlant;
-  root["soilMosture"] =  soilMoistureSensorValue;
-  root["humidity"] = DHT.humidity;
-  root["temperature"] =  DHT.temperature;
+  root["soilMosture"] =  plantData.soilMoistureSensorValue;
+  root["humidity"] = plantData.DHT.humidity;
+  root["temperature"] = plantData.DHT.temperature;
   String data;
   root.printTo(data);
-  
+  return data;
+}
+
+String preparePutRequest(String data) {
   String putRequest = "PUT " + PATH  + " HTTP/1.1\r\n" +
                        "Host: " + HOST + ":" + PORT + "\r\n" +
                        "Content-Type: application/json\r\n" +
                        "Content-Length: " + data.length() + "\r\n\r\n" + 
                        data;
-
-  sendCommand("AT+CIPMUX=1",5,"OK");
-  sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
-  sendCommand("AT+CIPSEND=0," +String(putRequest.length()),4,">");
-  esp8266.println(putRequest);Serial.println(putRequest);delay(1500);countTrueCommand++;
-  sendCommand("AT+CIPCLOSE=0",5,"OK");
-
- 
-  delay(1000);
-  digitalWrite(7, LOW);
-  delay(1000);
-  digitalWrite(7, HIGH);
- 
+  return putRequest;
 }
 
-int getSensorData() {
-  return random(1000); // Replace with 
+void sendPutRequest(String putRequest) {
+  sendCommand("AT+CIPMUX=1",5,"OK");
+  
+  sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+  
+  sendCommand("AT+CIPSEND=0," +String(putRequest.length()),4,">");
+  
+  esp8266.println(putRequest);Serial.println("\n" + putRequest + "\n");delay(1500);countTrueCommand++;
+  
+  sendCommand("AT+CIPCLOSE=0",5,"OK");
 }
 
 void sendCommand(String command, int maxTime, char readReplay[]) {
@@ -111,5 +151,17 @@ void sendCommand(String command, int maxTime, char readReplay[]) {
   }
   
   found = false;
- }
+}
+
+boolean getClientStatus() {
+  boolean clientStatus;
+
+  return clientStatus;
+}
  
+void water() {
+  delay(1000);
+  digitalWrite(WATER_PIN, LOW);
+  delay(1000);
+  digitalWrite(WATER_PIN, HIGH); 
+}
